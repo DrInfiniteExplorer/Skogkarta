f3e51b347aca9587c15ab09798e098e0be66d169
Pierre LeMoine <git@luben.se>
2016-04-11 22:42:09 +0200

    Changed front page to interpolate without escaping, and also from html file.

--- npm-host/routes/index.js
+++ npm-host/routes/index.js
@@ -5,14 +5,13 @@ var fs = require('fs');
 
 var areas = require(path.join(__dirname, '../public/areas.json'))
 
-var newsPath = path.join(__dirname, '../../news.json')
+var newsPath = path.join(__dirname, '../../news.html')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   fs.readFile(newsPath, 'utf8', function (err, data) {
-    if (err) data = "[]";
-    data = JSON.parse(data);
-    if(!data) data = []
+    if (err) data = "";
+    if(!data) data = ""
     res.render('index', { areas: areas , news: data});
   });
 });
